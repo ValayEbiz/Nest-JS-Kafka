742b51dccf162f04978c79aa41c834462ccbed06
ValayEbiz <valay.andhariya@esparkbizmail.com>
2025-11-24 13:31:28 +0530

    successfully handle the payment notification in notification service from topic payment-succeed

--- apps/notification/src/app/app.controller.ts
+++ apps/notification/src/app/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @EventPattern('payment-succeed')
+  handlePaymentSucceed(@Payload() order: any) {
+    console.log('Notification Sent for successful payment ' + order.paymentId);
   }
 }
